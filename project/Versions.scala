8faaa89ba19361185a269219e4b55c9614ff9fd0
Nicolas Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2015-05-22 12:36:15 -0700

    MBEE-200: Continuous Integration Refactoring

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@ object Versions {
   val scala = "2.11.6"
 
   // JPL Ontology Modeling Framework Core
-  val jpl_omf_core = "1800.02-41a9519c5eb676694a4491981737408ae7bc5e13-SNAPSHOT"
+  val jpl_omf_core = "1800.02-76a10a81ecb77c1c765151ab34ea59a9f868c69c"
 
   // JPL MBEE Common Scala Libraries
   val jpl_mbee_common_scala_libraries_revision="650c69e6e7defc0b3e430d9c3cb290e8b3cc1f88"
