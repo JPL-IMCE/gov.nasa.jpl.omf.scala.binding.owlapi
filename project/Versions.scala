acc88e73f6aea6fd8dfcc6522b3f13378f2c80e7
Nicolas Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2015-05-23 15:08:24 -0700

    MBEE-207: add LOADPROD as artifact dependency

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,11 +1,17 @@
 object Versions {
   val scala = "2.11.6"
 
+  // JPL MBEE release
+  val jpl_mbee_release_prefix="1800.02-"
+
   // JPL Ontology Modeling Framework Core
-  val jpl_omf_core = "1800.02-76a10a81ecb77c1c765151ab34ea59a9f868c69c"
+  val jpl_omf_core = jpl_mbee_release_prefix+"76a10a81ecb77c1c765151ab34ea59a9f868c69c"
 
   // JPL MBEE Common Scala Libraries
   val jpl_mbee_common_scala_libraries_revision="650c69e6e7defc0b3e430d9c3cb290e8b3cc1f88"
-  val jpl_mbee_core = "1800.02-"+jpl_mbee_common_scala_libraries_revision
-  val jpl_owlapi = "1800.02-"+jpl_mbee_common_scala_libraries_revision
+  val jpl_mbee_core = jpl_mbee_release_prefix+jpl_mbee_common_scala_libraries_revision
+  val jpl_owlapi = jpl_mbee_release_prefix+jpl_mbee_common_scala_libraries_revision
+
+  // IMCE-LOADPROD
+  val imce_loadprod=jpl_mbee_release_prefix+"4e67e5645272d2effb48f11742b736c447af92e9"
 }
