0acff549183c83f9b85fe4cf7d7db941e702468d
Nicolas Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2015-05-24 17:50:02 -0700

    MBEE-210 dependencies to OMF/Core individual library artifacts

--- project/Versions.scala
+++ project/Versions.scala
@@ -5,7 +5,7 @@ object Versions {
   val jpl_mbee_release_prefix="1800.02-"
 
   // JPL Ontology Modeling Framework Core
-  val jpl_omf_core = jpl_mbee_release_prefix+"76a10a81ecb77c1c765151ab34ea59a9f868c69c"
+  val jpl_omf_core = jpl_mbee_release_prefix+"b3ffcd1655d68330df64cdf9c949879fc41fa86c"
 
   // JPL MBEE Common Scala Libraries
   val jpl_mbee_common_scala_libraries_revision="650c69e6e7defc0b3e430d9c3cb290e8b3cc1f88"
