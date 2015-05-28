ef9607a7b1129a2955cafe7148d2ff158afed31e
Nicolas Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2015-05-28 14:53:02 -0700

    MBEE-209: dependencies update

--- project/Versions.scala
+++ project/Versions.scala
@@ -7,6 +7,10 @@ object Versions {
   // JPL Ontology Modeling Framework Core
   val jpl_omf_core = jpl_mbee_release_prefix+"e8ef32c064d457923beecfce50a4359a33c1aa22"
 
+  // JPL MBEE Common Scala Libraries
+  val jpl_mbee_common_scala_libraries_revision="60d5d55571aeff7e4fb884ecd8d4e02b0da27ce6"
+  val jpl_owlapi = jpl_mbee_release_prefix+jpl_mbee_common_scala_libraries_revision
+
   // IMCE-LOADPROD
   val imce_loadprod=jpl_mbee_release_prefix+"52e7f929d0b6ecbb55cd4bda24f9f724db1b7c28"
 }
