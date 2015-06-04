dac46c06c0f3a1df5d7862f159f064e0f88eba5c
Nicolas Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2015-06-04 11:01:03 -0700

    MBEE-209: JPL MBEE SBT plugin simplification: 47f5b520d315d9a17c2d350bee3203130d2f5930 (todo: enable ontology artifact dependency when imce-loadprod publishes to maven)

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,16 +1,11 @@
 object Versions {
-  val scala = "2.11.6"
-
-  // JPL MBEE release
-  val jpl_mbee_release_prefix="1800.02-"
 
   // JPL Ontology Modeling Framework Core
-  val jpl_omf_core = jpl_mbee_release_prefix+"e8ef32c064d457923beecfce50a4359a33c1aa22"
+  val jpl_omf_core_revision="4fd1bd01a1258d6e901e62a318a784a4d202ffff"
 
   // JPL MBEE Common Scala Libraries
-  val jpl_mbee_common_scala_libraries_revision="60d5d55571aeff7e4fb884ecd8d4e02b0da27ce6"
-  val jpl_owlapi = jpl_mbee_release_prefix+jpl_mbee_common_scala_libraries_revision
+  val jpl_mbee_common_scala_libraries_revision="fd6ef49108d43fad925a30b7a6dcc1d81b4b348d"
 
   // IMCE-LOADPROD
-  val imce_loadprod=jpl_mbee_release_prefix+"52e7f929d0b6ecbb55cd4bda24f9f724db1b7c28"
+  val imce_loadprod_revision = "52e7f929d0b6ecbb55cd4bda24f9f724db1b7c28"
 }
