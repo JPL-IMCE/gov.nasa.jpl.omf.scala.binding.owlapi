a76f6321373c74a52d4737a524a6bc23d0b784ab
Nicolas Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2015-06-05 13:16:33 -0700

    MBEE-209: MBEE Common Scala Libs: 576ddfdcdb17e9078f75a9ec8eef1336b82c5954 OMF Core: ebf205698c45de66709a722c9aa8fc47902bf400

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,10 +1,10 @@
 object Versions {
 
   // JPL Ontology Modeling Framework Core
-  val jpl_omf_core_revision="4fd1bd01a1258d6e901e62a318a784a4d202ffff"
+  val jpl_omf_core_revision="ebf205698c45de66709a722c9aa8fc47902bf400"
 
   // JPL MBEE Common Scala Libraries
-  val jpl_mbee_common_scala_libraries_revision="fd6ef49108d43fad925a30b7a6dcc1d81b4b348d"
+  val jpl_mbee_common_scala_libraries_revision="576ddfdcdb17e9078f75a9ec8eef1336b82c5954"
 
   // IMCE-LOADPROD
   val imce_loadprod_revision = "2ffe9b72b5914e8299f45a374c12f95604695229"
