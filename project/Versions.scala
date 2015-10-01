3aa883eb5fbbd60fe33669d79c2292fd772d71af
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2015-10-01 16:04:43 -0700

    IMCE-LOADPROD with temp. workaround for annotation ontology

--- project/Versions.scala
+++ project/Versions.scala
@@ -7,5 +7,5 @@ object Versions {
   val jpl_mbee_common_scala_libraries_revision="5e73616ed22dd4b6fe935b1ddd4a9bf28e0a8dd4"
 
   // IMCE-LOADPROD
-  val imce_loadprod_revision = "7c2d8e95ae7570aabc6c7d9454773548623b457f"
+  val imce_loadprod_revision = "1aad8531304e20dd6cf254d271deca29eaf7cebe"
 }
