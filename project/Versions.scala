07cc900636bf3950813d199b32d71f2a0fb8d465
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2015-11-04 12:46:12 -0800

    OMF/Core & Scala libraries (OWLAPI 4.1.1)

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,10 +1,10 @@
 object Versions {
 
   // JPL Ontology Modeling Framework Core
-  val jpl_omf_core_revision="6aab7d74ebd60a1cf2dbe06ed891680f895569ed"
+  val jpl_omf_core_revision="1cdc8c3528e3eb7227670d7f8435a16ef8627af5"
 
   // JPL MBEE Common Scala Libraries
-  val jpl_mbee_common_scala_libraries_revision="202d290d17805d26522329ceb8c09fe5945ffebf"
+  val jpl_mbee_common_scala_libraries_revision="d44cf0591f31cb1005a9b4a2fab47bfd235e005c"
 
   // IMCE-LOADPROD
   val imce_loadprod_revision = "1aad8531304e20dd6cf254d271deca29eaf7cebe"
