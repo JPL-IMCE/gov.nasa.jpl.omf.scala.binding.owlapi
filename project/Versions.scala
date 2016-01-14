e4fd6f5397e24bacbf25e551cdcec5588e2a696b
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2016-01-14 14:36:09 +0000

    dependencies

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,14 +1,14 @@
 object Versions {
 
   // this project
-  val version="1.4"
+  val version="1.5"
 
   // JPL Ontology Modeling Framework Core
-  val jpl_omf_core="1.4"
+  val jpl_omf_core="1.6"
 
   // JPL MBEE Common Scala Libraries
-  val jpl_mbee_common_scala_libraries="1.9"
+  val jpl_mbee_common_scala_libraries="1.11"
 
   // IMCE-LOADPROD
-  val ontologies="1.2"
+  val ontologies="1.3"
 }
